661bf999123464a20e68c1e27a7a394478e4ab2f
George Hurst <114651221+GeorgeHurst@users.noreply.github.com>
2025-08-28 10:23:08 +0100

    Update dispense_test.py

--- [DEV] Test Files/dispense_test.py
+++ [DEV] Test Files/dispense_test.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.abspath('venv/lib/python3.11/site-packages'))
 from adafruit_motor import servo
 from adafruit_pca9685 import PCA9685
 import board
+from gpiozero import Button
 
 import time
 
@@ -16,12 +17,17 @@ PCA.frequency = 50
 # Servos
 servos = servo.Servo(PCA.channels[0], min_pulse=500, max_pulse=2500)
 
+# Sensor
+sensor = Button(19)
+
+
 while True:
 	servo.angle = 0
 	for i in range(90):
 		servo.angle = i*2
 		time.sleep(0.025)
 	time.sleep(0.5)
-	while not detected:
-		#blah blah
+	while not sensor.is_pressed:
+		#wait
+		time.sleep(0.01)
 	servo.angle = 0
