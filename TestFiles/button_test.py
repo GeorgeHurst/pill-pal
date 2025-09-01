95231439688a36cc6c86968bfe5a4078ae1fcec8
George Hurst <114651221+GeorgeHurst@users.noreply.github.com>
2025-09-01 14:00:27 +0100

    Update button_test.py

--- TestFiles/button_test.py
+++ TestFiles/button_test.py
@@ -1,14 +1,17 @@
-from gpiozero import Button
+from gpiozero import Button, LED
 import sys, os
 
 sys.path.append(os.path.abspath('venv/lib/python3.11/site-packages'))
 
-pin = 0x1A
 
-button = Button(pin)
+
+button = Button(26)
+led = LED(22)
 
 while True:
   if button.is_pressed:
     print("    pressed")
+    led.on()
   else:
     print("unpressed")
+    led.off()
