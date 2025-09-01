52a4e0ddad43f499cae07f222dc2f3a34206a2dd
George Hurst <114651221+GeorgeHurst@users.noreply.github.com>
2025-09-01 13:43:49 +0100

    Update button_test.py

--- TestFiles/button_test.py
+++ TestFiles/button_test.py
@@ -1,1 +1,13 @@
+from gpiozero import Button
 
+sys.path.append(os.path.abspath('venv/lib/python3.11/site-packages'))
+
+pin = 0xFFF
+
+button = Button(pin)
+
+while True:
+  if sensor.is_pressed:
+    print("    pressed")
+  else:
+    print("unpressed")
