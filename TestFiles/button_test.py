a5dfb1a035cc2f6760291fba0d7ff7dc861219d8
George Hurst <114651221+GeorgeHurst@users.noreply.github.com>
2025-09-01 14:13:49 +0100

    Update button_test.py

--- TestFiles/button_test.py
+++ TestFiles/button_test.py
@@ -1,9 +1,10 @@
 from gpiozero import Button, LED
 import sys, os
+sys.path.insert(0, "/home/pi/pill-pal/venv/lib/python3.11/site-packages")
 from adafruit_pca9685 import PCA9685
 
 
-sys.path.append(os.path.abspath('venv/lib/python3.11/site-packages'))
+# sys.path.append(os.path.abspath('venv/lib/python3.11/site-packages'))
 
 buzzer = pca.channels[15]
 
