5c1b72d7eb11d5157cd142f4d1eeb20b366833cf
George Hurst <114651221+GeorgeHurst@users.noreply.github.com>
2025-09-01 14:11:40 +0100

    Update button_test.py

--- TestFiles/button_test.py
+++ TestFiles/button_test.py
@@ -2,9 +2,8 @@ from gpiozero import Button, LED
 import sys, os
 from adafruit_pca9685 import PCA9685
 
-sys.path.append(os.path.abspath('venv/lib/python3.11/site-packages'))
 
-PCA_CONTROLLER = PCA9685(I2C)
+sys.path.append(os.path.abspath('venv/lib/python3.11/site-packages'))
 
 buzzer = pca.channels[15]
 
