086b4453e2113fada1441dfd6e35f1532bfd08d6
George Hurst <114651221+GeorgeHurst@users.noreply.github.com>
2025-09-01 14:09:10 +0100

    Update button_test.py

--- TestFiles/button_test.py
+++ TestFiles/button_test.py
@@ -1,17 +1,22 @@
-from gpiozero import Button, LED
+from gpiozero import Button, LED. Buzzer
 import sys, os
+from adafruit_pca9685 import PCA9685
 
 sys.path.append(os.path.abspath('venv/lib/python3.11/site-packages'))
 
+PCA_CONTROLLER = PCA9685(I2C)
 
+buzzer = pca.channels[15]
 
 button = Button(26)
 led = LED(22)
 
 while True:
   if button.is_pressed:
+    buzzer.frequency = 400
     print("    pressed")
     led.on()
   else:
+    buzzer.frequency = 0
     print("unpressed")
     led.off()
