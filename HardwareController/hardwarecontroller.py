eac0f400d377974b82034528a267e10376d52be4
George Hurst <114651221+GeorgeHurst@users.noreply.github.com>
2025-09-08 09:42:29 +0100

    hardwarecontroller.py updated

--- HardwareController/hardwarecontroller.py
+++ HardwareController/hardwarecontroller.py
@@ -3,13 +3,13 @@ import requests
 from gpiozero import LED, Button, LineSensor
 from adafruit_motor import servo
 from adafruit_pca9685 import PCA9685
-# import board
+import board
 
 # from .ServerManagers.settings import config
 
 
-schedule = requests.get(f'http://127.0.0.1:5000/api/get/schedule')
-print(schedule.json())
+# schedule = requests.get(f'http://127.0.0.1:5000/api/get/schedule')
+# print(schedule.json())
 
 
 """
@@ -21,31 +21,30 @@ print(schedule.json())
 1x RTC
 """
 
-# I2C = board.I2C()
+I2C = board.I2C()
 
-# # PWM Controller
-# PCA_CONTROLLER = PCA9685(I2C)
+# PWM Controller
+PCA_CONTROLLER = PCA9685(I2C)
 
-# # Buttons
-# RELEASE_DOSE = Button(26) # Physical user input to release dose
-# button2 = Button(16)
-# button3 = Button(12)
-# button4 = Button(25)
+# Buttons
+RELEASE_DOSE = Button(26) # Physical user input to release dose
+button2 = Button(16)
+button3 = Button(12)
+button4 = Button(25)
 
-# # Sensors
-# COMPARTMENT_SENSOR1 = LineSensor(19)
-# COMPARTMENT_SENSOR2 = LineSensor(13)
-# COMPARTMENT_SENSOR3 = LineSensor(6)
-# COMPARTMENT_SENSOR4 = LineSensor(5)
+# Sensors
+SENSORS = [ Button(19). Button(13), Button(6), Button(5) ]
 
-# # LEDs
-# DOSE_INDICATOR_LED = LED(4) # GREEN
-# LATEDOSE_LED = LED(17) # RED
-# led3 = LED(27)
-# led4 = LED(22)
+# LEDs
+DOSE_INDICATOR_LED = LED(4) # GREEN
+LATEDOSE_LED = LED(17) # RED
+led3 = LED(27)
+led4 = LED(22)
 
-# # Servos
-# COMPARTMENT_SERVO1 = servo.Servo(PCA_CONTROLLER.channels[0])
-# COMPARTMENT_SERVO2 = servo.Servo(PCA_CONTROLLER.channels[1])
-# COMPARTMENT_SERVO3 = servo.Servo(PCA_CONTROLLER.channels[2])
-# COMPARTMENT_SERVO4 = servo.Servo(PCA_CONTROLLER.channels[3])
+# Servos
+SERVOS = [    
+    servo.Servo(PCA.channels[0], min_pulse=500, max_pulse=2500),
+    servo.Servo(PCA.channels[1], min_pulse=500, max_pulse=2500),
+    servo.Servo(PCA.channels[2], min_pulse=500, max_pulse=2500),
+    servo.Servo(PCA.channels[3], min_pulse=500, max_pulse=2500)
+]
